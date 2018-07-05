c7fc26e45b0c14043fd2d7a1ea945ac6f50fb8cb
Victor Jimenez <vtorero@gmail.com>
2018-07-05 13:53:26 -0500

    rtp2

---
+++ WebApiIntranet/WebApiMovil/Models/Actividad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApiMovil.Models
+{[DataContract]
+    public class Actividad
+    {
+        [DataMember]
+        public int idActividad { get; set; }
+
+        [DataMember]
+        public int idProyecto { get; set; }
+
+        [DataMember]
+        public DateTime fechaInicio { get; set; }
+
+        [DataMember]
+        public DateTime fechaFin { get; set; }
+
+        [DataMember]
+        public string descripcion { get; set; }
+
+        [DataMember]
+        public int porcentajeReal { get; set; }
+
+        [DataMember]
+        public int porcentajePlan { get; set; }
+
+        [DataMember]
+        public string estado { get; set; }
+    }
+}
