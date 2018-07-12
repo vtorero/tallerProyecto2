8085f425f6a7ba725ce8f145d11a1f6d8568931c
Victor Jimenez <vjimenez@americatv.com.pe>
2018-07-12 17:50:18 -0500

    avance

---
+++ WebApiIntranet/WebApiMovil/Models/Solicitud.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApiMovil.Models
+{   [DataContract]
+    public class Solicitud
+{       [DataMember]
+        public int idSolicitud { get; set; }
+        [DataMember]
+        public int idProyecto { get; set; }
+        [DataMember]
+        public int idCoordinador { get; set; }
+        [DataMember]
+        public int idInspector { get; set; }
+        [DataMember]
+        public string observacion { get; set; }
+        [DataMember]
+        public DateTime fechaAprobacion { get; set; }
+        [DataMember]
+        public string estadoSolicitud { get; set; }
+    }
+}
