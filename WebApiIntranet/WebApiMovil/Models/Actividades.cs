150cce56067e9679d844764bf2a296e93b2e1c09
Victor Jimenez <vtorero@gmail.com>
2018-07-14 00:37:05 -0500

    a

---
+++ WebApiIntranet/WebApiMovil/Models/Actividades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApiMovil.Models
+{
+    [DataContract]
+    public class Actividades
+    {
+        [DataMember]
+        public int idActividad { get; set; }
+        [DataMember]
+        public string Descripcion { get; set; }
+    }
+}
